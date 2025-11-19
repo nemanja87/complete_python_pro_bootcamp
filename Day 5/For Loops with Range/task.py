a3c62342a503ccdfbb9a7443df88c51879490e92
nemo <moshorinski@gmail.com>
2025-11-19 07:39:28 +0100

    Day 5 practice.

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,15 @@
+sum = 0
+for number in range(1, 101):
+   sum += number
+
+print(sum)
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,4 @@
 fruits = ["Apple", "Peach", "Pear"]
+
+for item in fruits:
+    print(item)
