9254325aacb4fcb970034eac1653ecbc979a1d80
nemo <moshorinski@gmail.com>
2025-11-23 17:03:41 +0100

    Day 9 practice

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,31 @@
+import art
+
+print(art.logo)
+bids = {}
+input_next = "yes"
+name = ""
+bid = 0
+max_bid = 0
+winner = ""
 # TODO-1: Ask the user for input
+while input_next != "no":
+    name = input("Please type your name: ")
+    bid = float(input("Please type your bid $: "))
+    input_next = input("Is there someone else to bid? Type: yes or no ")
+
+    if input_next == "yes":
+        print("\n" * 100)
+
+    bids[name] = bid
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+for bid_value in bids:
+    current = bids[bid_value]
+    if current > max_bid:
+        max_bid = current
+        winner = bid_value
 
+
+print(f"The winner of the bid is {winner} with offer: {max_bid}")
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,27 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+grade = 0
+grade_value = ""
+
+for score in student_scores:
+    grade = student_scores[score]
+
+    if grade >= 91 and grade <= 100:
+        grade_value = "Outstanding"
+    elif grade >= 81 and grade <= 90:
+        grade_value = "Exceeds Expectations"
+    elif grade >= 71 and grade <= 80:
+        grade_value = "Acceptable"
+    else:
+        grade_value = "Fail"
+
+    student_grades[score] = grade_value
+
+print(student_grades)
